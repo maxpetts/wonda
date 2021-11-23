aa75608a4131a0836be1bf6fba6859001ba4480a
maxpetts <maxpetts9@gmail.com>
2021-11-23 20:36:13 +0000

    Initial Commit

---
+++ wonda/wondaApp.swift
@@ -0,0 +1,17 @@
+//
+//  wondaApp.swift
+//  wonda
+//
+//  Created by mp on 23/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct wondaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
